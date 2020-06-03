f19d82585caa2d3a7838859ee8b61f86490de35b
dytq <taariq.deda@gmail.com>
2020-06-03 08:06:50 +0200

    correction de res

--- feistel.c
+++ feistel.c
@@ -4,8 +4,7 @@ bc32 feistel(bc32 moitie_bloc, bc48 sous_cle, int ** pointeur) {
 	bc48 expansion = swap_bloc_32(moitie_bloc, pointeur[13]);
 	bc48 S = expansion ^ sous_cle;
 	bc32 res =  fonctionS(S,pointeur);
-	res = swap_bloc_32(res, pointeur[9]);
-	return res;
+	return swap_bloc_32(res, pointeur[9]);
 }
 
 // a paralliser
@@ -17,11 +16,11 @@ bc4 thread_fonction_s(bc6 s,int * pointeur) {
 
 bc32 fonctionS(bc48 text, int ** pointeur) {
 	bc32 res = 0;
-	bc48 masque = 0x0000003F; // masque initial (0x11111100000000...)
+	bc48 masque = 0x0000003F; // masque initial
 	for(int i = 0 ; i < 7 ; i++) {
 		masque = masque << 6*!(!i); // on déplace de 6 à gauche et on recupere la valeur
+		res = res << (4*!(!i));
 		res = res | thread_fonction_s((masque & text) >> 6*i,pointeur[2 + i]); 
-		res = res << 0x4;
 	}
 	return res;
-} 
+}
