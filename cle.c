fae620c891c9164005fff541b9c85245a186ba94
dytq <taariq.deda@gmail.com>
2020-06-05 11:27:41 +0200

    clean cle

--- cle.c
+++ cle.c
@@ -3,10 +3,12 @@
 void init_cles(char * mot_de_passe, bc_cle_s * B, int ** pointeur) {
 	bc64 * blocs;
 	int taille_cle = strlen(mot_de_passe);
+	if(taille_cle > 64) {
+		fprintf(stderr,"Mot de passe entrée trop grande\n");
+		exit(EXIT_FAILURE);
+	}
 	blocs = convertir_message_64_bits(mot_de_passe,taille_cle,1);
-	blocs[0] = 0xAABB09182736CCDD;
-	printf("init cle : %lX\n",blocs[0]);
-	blocs[0] = reverse_64_bits(blocs[0]);
+	*blocs = reverse_64_bits(*blocs);
 	*B = permutation_initial_cle(*(blocs), pointeur);
 	free(blocs);
 }
--- cle.h
+++ cle.h
@@ -1,23 +1,39 @@
 #ifndef _cle_h
 #define _cle_h
 
-#include "bloc.h"
-#include "interface.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-// Bloc pour la clé de 48 bits
+#include "bloc.h"
+
+/* Bloc de clé de 56 bits composé de 28 bits pour gauche et droite */ 
 typedef struct {
 	bc28 gauche;
 	bc28 droite;
 } bc_cle_s;
 
-//~ void hachage() -> c'est pour la fonction de hachage si j'ai envie de la faire...
-
+/**
+ * Permet d'initialiser une paire de clé
+ * @param le mot de passe entré par l'utilisateur < 64 bits
+ * @param la cle à retourner
+ * @param le pointeur vers les tableaux de swapping
+ * */
 void init_cles(char * mot_de_passe, bc_cle_s * B, int ** pointeur);
 
+/**
+ * Permet d'effectuer la première permutation PC-1 pour transformé un 64 bits en 56 bits
+ * @param la cle de 64 bits
+ * @param le pointeur vers les tableaux de swapping
+ * @return la clé de 56 bits
+ * */
 bc_cle_s permutation_initial_cle(bc64 cle, int ** pointeur);
 
+/**
+ * Permet de generer une clé de 48 bits
+ * @param la cle des deux branches de 32 bits
+ * @param le pointeur vers le tableaux de swapping PC-2
+ * @return une clée de 48 bits a xor(er) message
+ * */ 
 bc48 genere_cle_48_bits(bc_cle_s * B,int * pointeur);
 
 #endif // _cle_h
